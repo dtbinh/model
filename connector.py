f47a7dd2f1f1362cfac38c422d7904d12be654a1
lijiaxuan <18392886693@163.com>
2016-01-29 19:22:25 +0800

    cps

--- connector.py
+++ connector.py
@@ -1,8 +1,39 @@
 __author__ = 'sapphire'
 from pymongo import MongoClient,collection,database
+import  random
+
+import xml.dom.minidom
+def writexml(data):
+    V = random.random() * 2 + data['va']
+    I = data['pd'] / V
+    DOMTree = xml.dom.minidom.parse("pdata.xml")
+    tree = DOMTree.documentElement
+    for i in range(6):
+        devices = tree.getElementsByTagName("DEVICE_" + str(i + 1))
+        items = devices[0].getElementsByTagName("item")
+        for item in items:
+            if item.getAttribute('D_LABEL') == 'Inst Van':
+                item.setAttribute('D_VALUE',str(V + random.random() * 2) + 'k')
+            elif item.getAttribute('D_LABEL') == 'Inst Vbn':
+                item.setAttribute('D_VALUE',str(V + random.random() * 2) + 'k')
+            elif item.getAttribute('D_LABEL') == 'Inst Vcn':
+                item.setAttribute('D_VALUE',str(V + random.random() * 2) + 'k')
+            elif item.getAttribute('D_LABEL') == 'Inst Ia':
+                item.setAttribute('D_VALUE',str(I + random.random() * 2))
+            elif item.getAttribute('D_LABEL') == 'Inst Ib':
+                item.setAttribute('D_VALUE',str(I + random.random() * 2))
+            elif item.getAttribute('D_LABEL') == 'Inst Ic':
+                item.setAttribute('D_VALUE',str(I + random.random() * 2))
+        f = open("pdata_x.xml",'w')
+        f.write(DOMTree.toxml())
+        f.close()
+
 con=MongoClient("localhost",27017)
 db=database.Database(con,'mydb')
 def insert_bus_data(data):
     db.model.insert_one(data)
+
+
 def update_bus_data(no,data):
     db.model.replace_one({'bus_id':no},data,True)
+    writexml(data)
--- smartmeter/smartmeter.py
+++ smartmeter/smartmeter.py
@@ -12,10 +12,6 @@ FLAG=select.EPOLLET|select.EPOLLIN
 epoller=select.epoll()
 fd_socket={}
 import numpy as np
-from scipy import interpolate
-from pd_sample import pd_sample
-import datetime
-
 ynew=[]
 qnew=[]
 pdtype =np.dtype([('bus_id','<i4'),('pd','<f4'),('qd','<f4')],align=True)
@@ -38,6 +34,7 @@ def sock_init():
             sock.append(tmp)
         except Exception:
             os._exit(1)
+
 recvnum=0
 sock_init()
 power=[[97.6, 44.2], [30, 30], [322, 2.4], [500, 184], [30, 30], [30, 30], [233.8, 84], [522, 176.6], [6.5, -66.6], [30, 30], [30, 30], [8.53, 88], [30, 30], [30, 30], [320, 153], [329, 32.3], [30, 30], [158, 30], [30, 30], [680, 103], [274, 115], [30, 30], [247.5, 84.6], [308.6, -92.2], [224, 47.2], [139, 17], [281, 75.5], [206, 27.6], [283.5, 26.9], [30, 30], [9.2, 4.6], [30, 30], [30, 30], [30, 30], [30, 30], [30, 30], [30, 30], [30, 30], [1104, 250]]
@@ -51,7 +48,7 @@ class worker1(threading.Thread):
             try:
                 recvnum=0
                 for i,k in enumerate(sock):
-                    a=np.array([(i,power[i][0]+random.random()*5.0,power[i][1]+3.0*random.random())],dtype=pdtype)
+                    a=np.array([(i+1,power[i][0]+random.random()*5.0,power[i][1]+3.0*random.random())],dtype=pdtype)
                     k.send(a.tobytes())
                 time.sleep(3.5)
             except Exception:
